4c0b730a98f8bdcb944b985bc4cce8df42d7604a
jhossepmartinez <jhossepmv@gmail.com>
2026-02-23 22:27:32 -0300

    chore: remove the d1 database

--- main.tf
+++ main.tf
@@ -15,16 +15,20 @@ provider "cloudflare" {
 module "worker" {
   for_each = { for worker in var.workers : worker.name => worker }
 
-  source             = "github.com/jhossepmartinez/terraform-cloudflare-worker?ref=v0.1.3"
+  source             = "github.com/jhossepmartinez/terraform-cloudflare-worker?ref=v0.2.0"
   account_id         = var.cloudflare_account_id
   compatibility_date = "2026-02-21"
   main_module        = "index.js"
   main_module_path   = "dist/index.js"
   name               = each.value.name
   tags               = ["temp", "hihi"]
-  bindings = [{
-    name = "GOOGLE_API_KEY"
-    type = "secret_text"
-    text = var.google_api_key
-  }]
+
+  bindings = [
+    {
+      name = "GOOGLE_API_KEY"
+      type = "secret_text"
+      text = var.google_api_key
+    }
+  ]
 }
+
