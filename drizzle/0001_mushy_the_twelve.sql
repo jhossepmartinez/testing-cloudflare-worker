0f58cee32cb299ecb141a9b53fba66efe581b5e5
jhossepmartinez <jhossepmv@gmail.com>
2026-02-26 20:04:52 -0300

    fix: add missing migration

---
+++ drizzle/0001_mushy_the_twelve.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `qa_history` ADD `username` text DEFAULT '[Missing username]' NOT NULL;--> statement-breakpoint
+ALTER TABLE `qa_history` ADD `sub` text DEFAULT '[Missing sub]' NOT NULL;
